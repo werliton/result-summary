2e93c2f3af0c7a40971fecc4fcc0bc74ba362954
werliton <werlitoncarlos@gmail.com>
2025-04-12 13:21:05 -0300

    feat: adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { Summary } from './components/summary/Summary'
 function App() {
 
   return (
-    <div className='w-full h-screen flex justify-center items-center'>
+    <div className='w-full h-screen flex flex-col justify-center items-center'>
       <main className="max-w-[375px] bg-white layout">
         <Result />
         <Summary />
       </main>
+      <footer className='mt-5 font-medium text-xs text-gray-600 opacity-40 hover:opacity-100'>Design by 
+        <a href="https://github.com/werliton/">@werliton</a></footer>
     </div>
   )
 }
